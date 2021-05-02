342340cc3ca97c78c3c6385d53f1bb70b511546c
Pablo <pablo.bollansee@gmail.com>
2021-05-02 22:12:41 +0200

    👌 Made it multithreaded (7 hours to 35 minutes!)

--- telaffuz.py
+++ telaffuz.py
@@ -4,41 +4,46 @@ import urllib.parse
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
 
-words = open('words.txt', 'r')
-output = open('output.txt', 'w')
+inputFile = 'words.txt'
+outputFile = 'output.txt'
 urlPrefix = 'https://sozluk.gov.tr/gts?ara='
+delimiter = '\t'
+threadCount = 16
 
-def writeColumn(data):
-    output.write(data)
-    output.write('\t')
+print(random.uniform(1, 2))
 
-def nextLine():
-    output.write('\n')
-
-for word in tqdm(words.readlines()):
+def processWord(word):
     word = re.split(',|/', word)[0].strip()
-    if not word: continue
     url = urlPrefix + urllib.parse.quote(word)
-    # print(url)
-
-    writeColumn(word)
+    processed = word
 
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read().decode())
-
-        # print(data)
-
         for info in data:
             if 'telaffuz' not in info:
-                writeColumn('error')
+                processed += delimiter + 'error'
             else:
                 pronunciation = info['telaffuz']
-                if not pronunciation: writeColumn('0')
-                else: writeColumn(pronunciation)
+                if not pronunciation: processed += delimiter + '0'
+                else: processed += delimiter + pronunciation
     
-    nextLine()
+    processed += '\n'
+    return processed
 
+def tryProcessWord(word):
+    for _ in range(5):
+        try:
+            return processWord(word)
+        except:
+            time.sleep(random.uniform(0.5, 1.5))
+            pass
+    return "failed\n"
 
+with Pool(threadCount) as pool:
+    with open(inputFile, 'r') as wordsFile:
+        words = wordsFile.readlines()
+        processedWords = tqdm(pool.imap(tryProcessWord, words), total=len(words))
+        # processedWords = tqdm(pool.map(tryProcessWord, words))
 
-words.close()
-output.close()
+    with open(outputFile, 'w') as output:
+        output.writelines(processedWords)
